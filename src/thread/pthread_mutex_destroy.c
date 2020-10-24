be3debb3b41fb04eb53cef6d3c131cd47dd20733
rcombs <rcombs@rcombs.me>
2020-10-24 01:17:32 -0500

    pthread_mutex_destroy: unlock still-locked mutexes passed in

--- src/thread/pthread_mutex_destroy.c
+++ src/thread/pthread_mutex_destroy.c
@@ -6,5 +6,16 @@ int pthread_mutex_destroy(pthread_mutex_t *mutex)
 	 * type (tracking ownership), it might be in the pending slot of a
 	 * robust_list; wait for quiescence. */
 	if (mutex->_m_type > 128) __vm_wait();
+
+	/* Some software seems to call destroy on locked mutexes.
+	 * This is undefined behavior, but it's harmless in glibc.
+	 * In our implementation, there's a risk that a destroyed mutex
+	 * could result in invalid internal state in the holding thread.
+	 * This is easy enough to handle, at least for the case where
+	 * the same thread owns the mutex.
+	 * We'll just attempt to unlock it, since this is harmless
+	 * in our implementation even if it's already unlocked,
+	 * or locked by another thread. */
+	__pthread_mutex_unlock(mutex);
 	return 0;
 }
